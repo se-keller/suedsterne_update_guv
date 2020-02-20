5e067b587f0c0cacd2f2f8a2edc25ce8337beb6c
se-keller <sebastian.sekell@googlemail.com>
2020-02-20 14:32:47 +0100

    mapper for guv entries

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,11 +1,33 @@
 import unittest
 
 
+class GuvEntry(object):
+    def __init__(self, customer):
+        self.customer = customer
+
+
 class CalendarService(object):
-    def entries_for(self, year, month):
+    def entries_for(self, year, month) -> [GuvEntry]:
+        if month == 3:
+            return [GuvEntry('TEST_KUNDE')]
         return []
 
 
-class FooTest(unittest.TestCase):
-    def test_foo(self):
+class CalendarServiceTest(unittest.TestCase):
+    def test_returns_empty_list_when_nothing_found(self):
         self.assertEqual([], CalendarService().entries_for(2020, 2))
+
+    def test_returns_single_entry(self):
+        self.assertEqual(GuvEntry('TEST_KUNDE').customer, CalendarService().entries_for(2020, 3)[0].customer)
+
+
+class GuvEntryMapper(object):
+    @classmethod
+    def from_json(cls, json) -> GuvEntry:
+        return GuvEntry('Zeppelin')
+
+
+class GuvEntryMapperTest(unittest.TestCase):
+    def test_maps_from_json(self):
+        guv_entry = GuvEntryMapper.from_json({'created': '2020-01-02T15:43:00.000Z', 'title': 'Kunde: Zeppelin'})
+        self.assertEqual(guv_entry.customer, 'Zeppelin')
